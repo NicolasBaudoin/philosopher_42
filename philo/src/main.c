a047ad90d9e37c6ea44493007c10440f961c4b26
nbaudoin <baudoin.nicolasg@gmail.com>
2026-04-27 12:58:18 +0200

    ok

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nbaudoin <nbaudoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 15:17:37 by nbaudoin          #+#    #+#             */
-/*   Updated: 2026/04/27 10:51:51 by nbaudoin         ###   ########.fr       */
+/*   Updated: 2026/04/27 11:57:27 by nbaudoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 int	main(int ac, char **av)
 {
 	if (input_not_valid(ac, av))
-	{
-		printf("ok cool\n");
 		return (1);
-	}
-	printf("oups\n");
 	return (0);
 }
